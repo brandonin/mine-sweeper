34f4011ed2357baaf7135b29c1e24f695983b247
Brandon In <brandonin@me.com>
2019-03-24 18:21:21 -0700

    removed unused function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Game from './Game';
 import './App.css';
 
 class App extends Component {
-  handleClick = (value) => {
-    console.log('hello', value)
-  }
-
   render() {
     return (
       <div className="App">
