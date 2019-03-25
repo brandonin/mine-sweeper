218e2c358e69fac6d3a8f620f81ac9b937ee31e9
Brandon In <brandonin@me.com>
2019-03-24 18:29:28 -0700

    simple change to remove the 0's with empty boxes

--- src/Square.js
+++ src/Square.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function Square(props) {
+    const value = props.value !== 0 ? props.value : '';
+
     return (
         <button className={`${props.className} square ${props.visible ? 'visible' : 'invisible'}`} onClick={props.onClick}>
             {props.visible &&
             <span className='positionAbsolute'>
-                { props.isBomb ? "💣" : props.value }
+                { props.isBomb ? "💣" : value }
             </span>}
         </button>
     )
